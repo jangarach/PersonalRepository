093c639ce19ef61b793b58e79e897be55ae9436c
janga <jangarach_98@mail.ru>
2021-03-25 10:46:29 +0700

    Добавил старую работу, проверки капчи

---
+++ Desktop-projects/CapchaWPF/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(109, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(391, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите Captcha:";
+            this.label3.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(144, 210);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 67);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(293, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(233, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(31, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "R";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 254);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Desktop-projects/CapchaWPF/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int l = 1;
+        private Random rand = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string s1 = textBox2.Text;
+            if (l < 3)
+            {
+                if (s != "admin" & s1 != "admin")
+                {
+                    MessageBox.Show("Неправильный пароль!");
+                    l++;
+                }
+                else
+                {
+                    MessageBox.Show("Вы успешо зашли");
+                    label3.Visible = false;
+                    textBox3.Visible = false;
+                }
+            }
+            else
+            {
+                label3.Visible = true;
+                textBox3.Visible = true;
+                button2.Visible = true;
+                button3.Visible = true;
+                button1.Enabled = false;
+                pictureBox1.Visible = true;
+                pictureBox1.Image.Dispose();
+                code = "";
+                CreateImage();
+            }
+            
+        }
+
+        private void CreateImage()
+        {
+            string code = GetRandomText();
+
+            Bitmap bitmap = new Bitmap(200, 50, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bitmap);
+            Pen pen = new Pen(Color.Yellow);
+            Rectangle rect = new Rectangle(0, 0, 200, 50);
+
+            SolidBrush b = new SolidBrush(Color.Black);
+            SolidBrush White = new SolidBrush(Color.White);
+
+            int counter = 0;
+
+            g.DrawRectangle(pen, rect);
+            g.FillRectangle(b, rect);
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                g.DrawString(code[i].ToString(), new Font("Georgia", 10 + rand.Next(14, 18)), White, new PointF(10 + counter, 10));
+                counter += 20;
+            }
+
+            DrawRandomLines(g);
+
+            if (File.Exists("d:/tempimage.bmp"))
+            {
+
+                try
+                {
+                    File.Delete("d:/tempimage.bmp");
+                    bitmap.Save("d:/tempimage.bmp");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+            else
+            {
+                bitmap.Save("d:/tempimage.bmp");
+
+            }
+
+            g.Dispose();
+            bitmap.Dispose();
+            pictureBox1.Image = Image.FromFile("d:/tempimage.bmp");
+
+        }
+        private void DrawRandomLines(Graphics g)
+        {
+            SolidBrush green = new SolidBrush(Color.Green);
+            //For Creating Lines on The Captcha
+            for (int i = 0; i < 20; i++)
+            {
+                g.DrawLines(new Pen(green, 2), GetRandomPoints());
+            }
+
+        }
+        private Point[] GetRandomPoints()
+        {
+            Point[] points = { new Point(rand.Next(10, 150), rand.Next(10, 150)), new Point(rand.Next(10, 100), rand.Next(10, 100)) };
+            return points;
+        }
+
+        string code;
+        private string GetRandomText()
+        {
+            StringBuilder randomText = new StringBuilder();
+
+            if (String.IsNullOrEmpty(code))
+            {
+                string alphabets = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+                Random r = new Random();
+                for (int j = 0; j <= 5; j++)
+                {
+
+                    randomText.Append(alphabets[r.Next(alphabets.Length)]);
+                }
+
+                code = randomText.ToString();
+            }
+
+            return code;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == code.ToString())
+            {
+                MessageBox.Show("Успешно!");
+                pictureBox1.Visible = false;
+                label3.Visible = false;
+                textBox3.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button1.Enabled = true;
+                l = 1;
+            }
+            else
+            {
+                MessageBox.Show("Неправильная Captcha");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateImage();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Dispose();
+            code = "";
+            CreateImage();
+        }
+    }
+}
