4bef6297c4167092ec681ff84a7920cb180cebfe
janga <jangarach_98@mail.ru>
2021-12-10 21:39:22 +0700

    Добавил Api для проекта Products

---
+++ Web-projects/ProductManagement/ProductManagementWebApp/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using ProductManagementWebApp.Models.DAL.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductManagementWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductService _productService = new ProductService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            _productService.GetProducts();
+            return View();
+        }
+    }
+}
---
+++ Web-projects/ProductManagement/ProductManagementWebApp/Models/DAL/Service/ProductService.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ProductManagementWebApp.Models.DAL.Service
+{
+    public interface IProductService
+    {
+        List<ProductViewModel> GetProducts();
+        ProductViewModel GetProduct(Guid id);
+        ProductViewModel UpdateProduct(Guid id, ProductViewModel product);
+        ProductViewModel AddProduct(ProductViewModel product); 
+    }
+    public class ProductService : IProductService
+    {
+        private static string baseUrl = "https://localhost:44317/api/";
+
+        public List<ProductViewModel> GetProducts()
+        {
+            List<ProductViewModel> products = new List<ProductViewModel>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                //HTTP GET
+                var responseTask = client.GetAsync("product");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+                    products = JsonConvert.DeserializeObject<List<ProductViewModel>>(readTask.Result);
+                }
+            }
+
+
+            return products;
+        }
+        public ProductViewModel AddProduct(ProductViewModel product)
+        {
+            var productJson = GetStringAsync(baseUrl + "users/");
+            //JsonConvert.DeserializeObject<ProductViewModel>(userJson);
+            return default;
+        }
+
+        public ProductViewModel GetProduct(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductViewModel UpdateProduct(Guid id, ProductViewModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Task<string> GetStringAsync(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return httpClient.GetStringAsync(url);
+            }
+        }
+    }
+}
